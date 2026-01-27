3be05b1f02a5a02f598c0262f71e4edf26ddf8fd
Dinh Thanh <dinhthanh11011@gmail.com>
2026-01-27 07:31:43 +0700

    add cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,21 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
   const frontendBaseUrl = configService.get<string>('frontendBaseUrl');
-
   app.enableCors({
     origin: [frontendBaseUrl],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
-    allowedHeaders: 'Content-Type, Authorization',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
   });
+
+  app.use(cookieParser());
 
   const appPort = configService.get<number>('port');
   await app.listen(appPort as number);
