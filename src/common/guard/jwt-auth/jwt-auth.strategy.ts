6ce1786a08c61e6de05be122d12dedbfe7732c8b
Dinh Thanh <dinhthanh11011@gmail.com>
2026-02-07 23:11:42 +0700

    format

--- src/common/guard/jwt-auth/jwt-auth.strategy.ts
+++ src/common/guard/jwt-auth/jwt-auth.strategy.ts
@@ -1,30 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import {
-  ExtractJwt,
-  FromRequestFunction,
-  Strategy,
-  StrategyOptions,
+    ExtractJwt,
+    FromRequestFunction,
+    Strategy,
+    StrategyOptions,
 } from 'passport-jwt';
 import { JwtAuthPayload } from '@/auth/dto/auth.dto';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtAuthStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly configService: ConfigService) {
-    super({
-      jwtFromRequest:
-        ExtractJwt.fromAuthHeaderAsBearerToken() as FromRequestFunction,
-      ignoreExpiration: false,
-      algorithms: ['RS256'],
-      secretOrKey: configService.get('jwt.secret') as string,
-    } as StrategyOptions);
-  }
+    constructor(private readonly configService: ConfigService) {
+        super({
+            jwtFromRequest:
+                ExtractJwt.fromAuthHeaderAsBearerToken() as FromRequestFunction,
+            ignoreExpiration: false,
+            algorithms: ['RS256'],
+            secretOrKey: configService.get('jwt.secret') as string,
+        } as StrategyOptions);
+    }
 
-  validate(payload: JwtAuthPayload) {
-    return {
-      userLoginId: payload.userLoginId,
-      jti: payload.jti,
-    };
-  }
+    validate(payload: JwtAuthPayload) {
+        return {
+            userLoginId: payload.userLoginId,
+            jti: payload.jti,
+        };
+    }
 }
