bb81d3e11cab2f71c47ac2a265fa73860228863a
Dinh Thanh <dinhthanh11011@gmail.com>
2026-02-11 11:34:14 +0700

    add examples for search

--- src/dictionary/dto/dctionary.dto.ts
+++ src/dictionary/dto/dctionary.dto.ts
@@ -7,15 +7,15 @@ export class DictionarySearchResultDto {
     @ApiProperty({ example: 'verb' })
     partOfSpeech: string;
 
-    @ApiProperty({
-        type: [String],
-        example: ['tích lũy,thu thập', 'tích lũy,tích tụ'],
-    })
-    meanings: string[];
+    @ApiProperty({ example: 'tích lũy,thu thập' })
+    meaning: string;
 
     @ApiProperty({
         description: 'URL to word image, or empty if none',
         example: 'https://cdn.langeek.co/photo/48239/original/?type=jpeg',
     })
     imageUrl: string;
+
+    @ApiProperty({ example: 'We need to accumulate the money.' })
+    examples: string[];
 }
