8e708a91b160ec7e1db64dbdd4e457a3620a9b70
Dinh Thanh <dinhthanh11011@gmail.com>
2026-06-13 19:19:52 +0700

    add health

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,13 @@ import { AppService } from '@/app.service';
 export class AppController {
     constructor(private readonly appService: AppService) {}
 
-    @Get('/ping')
-    async getHealth() {
+    @Get('ping')
+    ping() {
+        return this.appService.getHealth();
+    }
+
+    @Get('health')
+    getHealth() {
         return this.appService.getHealth();
     }
 }
